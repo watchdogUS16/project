26287e51075ff97a7d203251cef7b266d580c6d4
root <root@raspberrypi>
2017-03-06 15:25:37 +0000

    app.js final

--- app.js
+++ app.js
@@ -3,14 +3,32 @@ var dweetClient = require('./node_modules/node-dweetio/');
 var dweetio = new dweetClient();
 var schedule = require('./node_modules/node-schedule/');
 
-var j = schedule.scheduleJob('*/10 * * * * *', function(){
+var dngl = require("dngl");
+var device = new dngl("/dev/ttyUSB2");
+var jsonfile = require("jsonfile");
+var datos
+var datos1
+var j = schedule.scheduleJob('*/30 * * * * *', function(){
 
-	test = speedTest.visual({maxTime: 5000});
-	var modem_data = require(modem);
-	test.on('data', function(data) {
+		test = speedTest.visual({maxTime: 5000});
+		test.on('data', function(data) {
 
-		dweetio.dweet_for("watchdog16", {some:modem_data}, function(err, dweet){
+			datos = data;
+
+			device.once("data", function(data){
+
+				datos1 = jsonConcat(datos,data);
+
+				dweetio.dweet_for("watchdog16", {some:datos1}, function(err, dweet){
+				});
+			});
+			console.log("Test realizado con Exito!!!");
 		});
-		console.log("Test realizado con Exito!!!");
-	});
 });
+
+function jsonConcat(o1, o2) {
+ for (var key in o2) {
+  o1[key] = o2[key];
+ }
+ return o1;
+}
