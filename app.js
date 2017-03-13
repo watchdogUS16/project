d9860aabc74c6f5f79616eff8d5823c83ce50852
System Administrator <root@MacBook-Pro-de-Francisco.local>
2017-03-13 16:48:19 +0100

    reordering program

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ var shell = require('./node_modules/shelljs');
 var dngl = require("dngl");
 
 var dweetio = new dweetClient();
-var device = new dngl("/dev/ttyUSB2");
-//var datos;
+//var device = new dngl("/dev/ttyUSB2");
+var datos;
 
 var j = schedule.scheduleJob('*/45 * * * * *', function(){
+
+	var device = new dngl("/dev/ttyUSB2");
 
 		shell.exec("route del default gw 192.168.0.1 eth0");
 		shell.exec("route add default gw 10.64.64.64 ppp0");
@@ -16,20 +18,17 @@ var j = schedule.scheduleJob('*/45 * * * * *', function(){
 		test = speedTest({maxTime: 5000});
 		test.on('data', function(data) {
 
-			var datos = data;
-
-			device.once('data', function(data){
-
-				shell.exec("sleep 10");
-				shell.exec("route del default gw 10.64.64.64 ppp0");
-				shell.exec("route add default gw 192.168.0.1 eth0");
-				dweetio.dweet_for("watchdog16", {some:jsonConcat(datos,data)}, function(err, dweet){});
-
-
-
-			});
-
+			datos = data;
 			console.log("Test realizado con Exito!!!");
+
+		});
+
+		device.once('data', function(data){
+
+			shell.exec("sleep 10");
+			shell.exec("route del default gw 10.64.64.64 ppp0");
+			shell.exec("route add default gw 192.168.0.1 eth0");
+			dweetio.dweet_for("watchdog16", {some:jsonConcat(datos,data)}, function(err, dweet){});
 
 		});
 
@@ -47,7 +46,7 @@ var j = schedule.scheduleJob('*/45 * * * * *', function(){
 
 		});
 
-		
+
 
 });
 
