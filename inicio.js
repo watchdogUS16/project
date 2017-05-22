1c6e574e234e2c691819575e03a8c299071a5a47
watchdogtfg@gmail.com <watchdogtfg@gmail.com>
2017-05-22 19:25:45 +0200

    insert

--- inicio.js
+++ inicio.js
@@ -3,9 +3,9 @@ var db = dblite('/home/project/dbReports.sqlite');
 var now = new Date();
 var shell = require('./node_modules/shelljs');
 
-db.query('CREATE TABLE IF NOT EXISTS Report (idReport INTEGER PRIMARY KEY, currentDate DATE, codError INTEGER, RBStatus TEXT, imsi INTEGER, imei INTEGER, downloadSpeed DOUBLE, uploadSpeed DOUBLE, operator INTEGER, mode INTEGER, time FLOAT, stat INTEGER, lac INTEGER, cell INTEGER, signal INTEGER)');
-db.query('INSERT INTO Report VALUES(null,?,?,?,?,?,?,?,?,?,?,?,?,?,?)', [now,-1,null,null,null,null,null,null,null,null,null,null,null,null,null]);
+	db.query('CREATE TABLE IF NOT EXISTS Report (idReport INTEGER PRIMARY KEY, currentDate DATE, codError INTEGER, RBStatus TEXT, imsi INTEGER, imei INTEGER, downloadSpeed DOUBLE, uploadSpeed DOUBLE, operator INTEGER, mode INTEGER, time FLOAT, stat INTEGER, lac INTEGER, cell INTEGER, signal INTEGER)');
+	db.query('INSERT INTO Report VALUES(null,?,?,?,?,?,?,?,?,?,?,?,?,?,?)', [now,-1,null,null,null,null,null,null,null,null,null,null,null,null,null]);
 
-shell.exec("sleep 30")
-shell.exec("sudo wvdial");
-shell.exec("sudo /etc/init.d/cron start");
+		shell.exec("sleep 30")
+		shell.exec("sudo wvdial");
+		shell.exec("sudo /etc/init.d/cron start");
