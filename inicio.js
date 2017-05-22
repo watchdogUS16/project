cc405665b4fee268e823a5c8de2b95d17af1951e
System Administrator <root@MacBook-Pro-de-Francisco.local>
2017-05-22 16:58:11 +0200

    inicio unificado

--- inicio.js
+++ inicio.js
@@ -1,6 +1,10 @@
 var dblite = require('./node_modules/dblite');
 var db = dblite('/home/project/dbReports.sqlite');
 var now = new Date();
+var shell = require('./node_modules/shelljs');
+
+shell.exec("sudo wvdial");
+shell.exec("sudo /etc/init.d/cron start");
 
 db.query('CREATE TABLE IF NOT EXISTS Report (idReport INTEGER PRIMARY KEY, currentDate DATE, codError INTEGER, RBStatus TEXT, imsi INTEGER, imei INTEGER, downloadSpeed DOUBLE, uploadSpeed DOUBLE, operator INTEGER, mode INTEGER, time FLOAT, stat INTEGER, lac INTEGER, cell INTEGER, signal INTEGER)');
 db.query('INSERT INTO Report VALUES(null,?,?,?,?,?,?,?,?,?,?,?,?,?,?)', [now,-1,null,null,null,null,null,null,null,null,null,null,null,null,null]);
